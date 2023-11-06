517567bf6c530aa4dd055dfbf0d8881d6b88b9e3
Oscar de la Hera Gomez <oscar@delasign.com>
2023-11-06 09:48:19 -0500

    How to create a ThreeJS responsive ReactJS project

--- src/App.tsx
+++ src/App.tsx
@@ -1,130 +1,23 @@
-import React from "react";
+// MARK: NPM Modules
+import React, { useRef, useEffect } from "react";
 import styled from "styled-components";
+import * as THREE from "three";
+// MARK: CSS
 import "./App.css";
 
+// MARK: Redux
 import { RootState } from "redux-functionality";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  increment,
-  decrement,
-  setCounter,
-  incrementByAmount,
-} from "redux-functionality/slices/counterSlice";
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100vw;
-  height: 100vh;
-`;
-
-const CounterContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 20px;
-  color: black;
-  font-size: 18px;
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  gap: 20px;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-`;
-
-const Button = styled.div`
-  color: black;
-  font-size: 18px;
-  width: 200px;
-  height: 60px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border: 1px solid black;
-  border-radius: 8px;
-  cursor: pointer;
-  color: black;
-  background-color: white;
-
-  :hover {
-    color: white;
-    background-color: black;
-  }
-`;
-
-const Input = styled.input`
-  display: flex;
-  flex-direction: row;
-  gap: 20px;
-  align-items: center;
-  justify-content: center;
-  height: 60px;
-  width: 100%;
-  color: black;
-  font-size: 18px;
-`;
+// MARK: Types
+// MARK: Components
+import Scene from "components/3JS";
+// MARK: Shaderss
+// MARK: Functionality
+// MARK: Utils
+// MARK: Styled Components
 
 function App() {
-  const counter: number = useSelector(
-    (state: RootState) => state.counter.value
-  );
-  const dispatch = useDispatch();
-  return (
-    <Container>
-      <CounterContainer>
-        <p>
-          Counter Value : <b>{counter}</b>
-        </p>
-        <br />
-        <ButtonContainer>
-          <Button
-            onClick={() => {
-              dispatch(increment());
-            }}
-          >
-            Add 1
-          </Button>
-          <Button
-            onClick={() => {
-              dispatch(decrement());
-            }}
-          >
-            Subtract 1
-          </Button>
-          <Button
-            onClick={() => {
-              dispatch(incrementByAmount(10));
-            }}
-          >
-            Add 10
-          </Button>
-          <Button
-            onClick={() => {
-              dispatch(incrementByAmount(-10));
-            }}
-          >
-            Subtract 10
-          </Button>
-        </ButtonContainer>
-        <br />
-        <ButtonContainer>
-          <p>Or set it here</p>
-          <Input
-            value={counter}
-            onChange={(ev) => {
-              const newValue = ev.target.value;
-              dispatch(setCounter(parseFloat(newValue)));
-            }}
-          />
-        </ButtonContainer>
-      </CounterContainer>
-    </Container>
-  );
+  return <Scene />;
 }
 
 export default App;
---
+++ src/components/3JS/index.tsx
@@ -0,0 +1,101 @@
+// MARK: NPM Modules
+import React, { useRef, useEffect } from "react";
+import styled from "styled-components";
+import * as THREE from "three";
+
+// MARK: Redux
+// MARK: Types
+// MARK: Components
+// MARK: Shaders
+// MARK: Functionality
+// MARK: Utils
+
+// MARK: Styled Components
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  overflow: none;
+`;
+
+// MARK: React Component
+// Props
+interface Props {}
+
+// Component
+const Scene = ({}: Props) => {
+  // MARK: Refs
+  const containerRef: any = useRef(null);
+
+  // MARK: State Variables
+  // MARK: Use Effects
+
+  useEffect(() => {
+    // componentDidMount events
+    // Render the scene
+    renderScene();
+    // Add the resize listener
+    window.addEventListener("resize", onWindowResize, false);
+
+    return () => {
+      // componentWillUnmount events
+      // Make sure to remove the renderer from the container, to avoid ThreeJS drawing an additional canvas everytime you make changes to the code.
+      containerRef.current.removeChild(renderer.domElement);
+      // Remove the event listener
+      window.removeEventListener("resize", onWindowResize, false);
+    };
+  }, []);
+
+  // MARK: Variables
+  const scene = new THREE.Scene();
+  const renderer = new THREE.WebGLRenderer();
+  const camera = new THREE.PerspectiveCamera();
+
+  // MARK: Functionality
+  const renderScene = () => {
+    // Clear the Scene
+    scene.clear();
+    // Create a scene, camera, and renderer
+    camera.fov = 75;
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.near = 0.1;
+    camera.far = 1000;
+    // Set up the renderer
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    containerRef.current.appendChild(renderer.domElement);
+
+    // Create a plane that matches the camera view
+    const planeGeometry = new THREE.PlaneGeometry(2, 2);
+    // Standard Material
+    const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+
+    // Add the Plane
+    scene.add(plane);
+
+    // Position the camera
+    camera.position.z = 5;
+    renderer.render(scene, camera);
+  };
+  // When the window resizes adapt the scene
+  const onWindowResize = () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  };
+
+  // Create an animation loop
+  const animate = () => {
+    requestAnimationFrame(animate);
+    renderer.clear();
+    renderer.render(scene, camera);
+  };
+
+  // MARK: Render
+  return <Container ref={containerRef}></Container>;
+};
+
+export default Scene;
