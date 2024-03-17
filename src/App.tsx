e660c2e055f3933eec6d4e2b3df3f2dd4e3f4247
Oscar de la Hera Gomez <oscar@delasign.com>
2024-03-17 12:25:17 -0400

    How to Outline Text in CSS

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import "./App.css";
 
-import { RootState } from "redux-functionality";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  increment,
-  decrement,
-  setCounter,
-  incrementByAmount,
-} from "redux-functionality/slices/counterSlice";
-
 const Container = styled.div`
   display: flex;
   justify-content: center;
@@ -19,110 +10,42 @@ const Container = styled.div`
   height: 100vh;
 `;
 
-const CounterContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 20px;
-  color: black;
-  font-size: 18px;
-`;
+interface OutlinedLetterProps {
+  strokeColor: string;
+  strokeWidth: number;
+}
 
-const ButtonContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  gap: 20px;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-`;
+// Answer from Ryall (https://stackoverflow.com/users/147731/ryall)
+// https://stackoverflow.com/questions/26634201/add-stroke-around-text-on-the-outside-with-css
 
-const Button = styled.div`
-  color: black;
-  font-size: 18px;
-  width: 200px;
-  height: 60px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border: 1px solid black;
-  border-radius: 8px;
-  cursor: pointer;
-  color: black;
-  background-color: white;
-
-  :hover {
-    color: white;
-    background-color: black;
-  }
-`;
-
-const Input = styled.input`
-  display: flex;
-  flex-direction: row;
-  gap: 20px;
-  align-items: center;
-  justify-content: center;
-  height: 60px;
-  width: 100%;
-  color: black;
-  font-size: 18px;
+const OutlinedLetter = styled.p<OutlinedLetterProps>`
+  font-family: -apple-system, system-ui, BlinkMacSystemFont;
+  font-size: 140px;
+  font-style: normal;
+  font-weight: 600;
+  margin-block-start: 0px;
+  margin-block-end: 0px;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  color: white;
+  text-shadow: ${({ strokeColor, strokeWidth }) => 
+  "-"+strokeWidth+"px -"+strokeWidth+"px  0 "+strokeColor+", "
+  + "0px -"+strokeWidth+"px 0 "+strokeColor+", "
+  + ""+strokeWidth+"px -"+strokeWidth+"px  0 "+strokeColor+", "
+  + ""+strokeWidth+"px 0px  0 "+strokeColor+", "
+  + ""+strokeWidth+"px "+strokeWidth+"px  0 "+strokeColor+", "
+  + "0px "+strokeWidth+"px  0 "+strokeColor+", "
+  + "-"+strokeWidth+"px "+strokeWidth+"px  0 "+strokeColor+", "
+  + "-"+strokeWidth+"px 0px  0 "+strokeColor+""};
+  paint-order: fill stroke; 
 `;
 
 function App() {
-  const counter: number = useSelector(
-    (state: RootState) => state.counter.value
-  );
-  const dispatch = useDispatch();
   return (
     <Container>
-      <CounterContainer>
-        <p>
-          Counter Value : <b>{counter}</b>
-        </p>
-        <br />
-        <ButtonContainer>
-          <Button
-            onClick={() => {
-              dispatch(increment());
-            }}
-          >
-            Add 1
-          </Button>
-          <Button
-            onClick={() => {
-              dispatch(decrement());
-            }}
-          >
-            Subtract 1
-          </Button>
-          <Button
-            onClick={() => {
-              dispatch(incrementByAmount(10));
-            }}
-          >
-            Add 10
-          </Button>
-          <Button
-            onClick={() => {
-              dispatch(incrementByAmount(-10));
-            }}
-          >
-            Subtract 10
-          </Button>
-        </ButtonContainer>
-        <br />
-        <ButtonContainer>
-          <p>Or set it here</p>
-          <Input
-            value={counter}
-            onChange={(ev) => {
-              const newValue = ev.target.value;
-              dispatch(setCounter(parseFloat(newValue)));
-            }}
-          />
-        </ButtonContainer>
-      </CounterContainer>
+      <OutlinedLetter strokeWidth={1} strokeColor={"black"}>
+        Aa
+      </OutlinedLetter>
     </Container>
   );
 }
